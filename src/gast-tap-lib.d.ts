93bcc59f1081e1ca3be38b31944ee396d128f5ae
Ben Little <ben.little6@gmail.com>
2020-04-28 19:58:39 -0600

    Type definitions (#25)
    * Add type definitions
    
    * Fix GasTap factory type signature
    
    * Unify formatting

---
+++ src/gast-tap-lib.d.ts
@@ -0,0 +1,26 @@
+
+declare namespace Gast {
+    export function GasTap(msg: string, test: (t: Session) => void): Session;
+    export interface Session {
+      counter      : number,
+      succCounter  : number,
+      failCounter  : number,
+      skipCounter  : number,
+      description  : string
+      ok           : (value: boolean, msg: string) => void,
+      notOk        : (value: boolean, msg: string) => void
+      equal        : (v1: any, v2: any, msg: string) => void,
+      notEqual     : (v1: any, v2: any, msg: string) => void,
+      deepEqual    : (v1: any, v2: any, msg: string) => void,
+      notDeepEqual : (v1: any, v2: any, msg: string) => void,
+      throws       : (fn: (...args: any) => any, msg: string) => void,
+      notThrow     : (fn: (...args: any) => any, msg: string) => void,
+      nan          : (v1: any, msg: string) => void,
+      notNan       : (v1: any, msg: string) => void,
+      skip         : (msg: string) => void,
+      pass         : (msg: string) => void,
+      fail         : (msg: string) => void,
+      reset        : () => void,
+    }
+}
+
